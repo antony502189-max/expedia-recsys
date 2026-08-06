1afe20bff93ae44894abf8098583fdccf24944d2
antony502189-max <antony502189@gmail.com>
2026-08-06 13:38:57 +0300

    Add product analytics package

---
+++ src/expedia_analytics/__init__.py
@@ -0,0 +1,5 @@
+"""Product analytics data marts for the Expedia dataset."""
+
+from expedia_analytics.builder import build_analytics, inspect_analytics, validate_analytics
+
+__all__ = ["build_analytics", "inspect_analytics", "validate_analytics"]
